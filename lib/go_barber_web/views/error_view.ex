3fddd5b7a18a6cf038565757ef175a847da6f486
João <joaopsramos231@gmail.com>
2021-05-21 20:50:46 -0300

    feat: error_view updated

--- lib/go_barber_web/views/error_view.ex
+++ lib/go_barber_web/views/error_view.ex
@@ -1,6 +1,8 @@
 defmodule GoBarberWeb.ErrorView do
   use GoBarberWeb, :view
 
+  import Ecto.Changeset, only: [traverse_errors: 2]
+
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
   # def render("500.html", _assigns) do
@@ -12,5 +14,21 @@ defmodule GoBarberWeb.ErrorView do
   # "Not Found".
   def template_not_found(template, _assigns) do
     Phoenix.Controller.status_message_from_template(template)
+  end
+
+  def render("error.json", %{reason: %Ecto.Changeset{} = changeset}) do
+    %{error: translate_errors(changeset)}
+  end
+
+  def render("error.json", %{reason: reason}) do
+    %{error: reason}
+  end
+
+  defp translate_errors(changeset) do
+    traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
   end
 end
