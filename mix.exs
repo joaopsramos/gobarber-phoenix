48fafaecfad91239cca0fe1f0de2fc3321125956
João <joaopsramos231@gmail.com>
2021-05-21 20:21:19 -0300

    feat: argon2_elixir dep added

--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule GoBarber.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.5"}
+      {:credo, "~> 1.5"},
+      {:argon2_elixir, "~> 2.0"}
     ]
   end
 
