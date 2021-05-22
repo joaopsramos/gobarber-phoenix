d10d4f074cc831ee51d7a1f5e8cfb6f6813ed8ea
João <joaopsramos231@gmail.com>
2021-05-22 00:40:35 -0300

    feat: frontend colors added

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -7,7 +7,17 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: "#FF9000",
+        white: "#F4EDE8",
+        gray: "#999591",
+        'gray-hard': "#666360",
+        shape: "#3E3B47",
+        'black-medium': "#28262E",
+        background: "#312E38",
+        inputs: "#232129"
+      },
   },
   variants: {
     extend: {},
