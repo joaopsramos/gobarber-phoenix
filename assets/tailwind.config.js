dbd82eb51fc23d55aeb487c43785400da9ae9650
João <joaopsramos231@gmail.com>
2021-05-24 14:59:33 -0300

    feat: red color added

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
         orange: "#FF9000",
         white: "#F4EDE8",
         gray: "#999591",
+        red: "#c53030",
         'gray-hard': "#666360",
         shape: "#3E3B47",
         'black-medium': "#28262E",
