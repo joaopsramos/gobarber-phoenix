284183995481a58f7a9264f31989fd80c924de10
João <joaopsramos231@gmail.com>
2021-05-24 23:53:29 -0300

    feat: fi_icon now accepts other attrs besides the class

--- lib/go_barber_web/helpers/icon_helper.ex
+++ lib/go_barber_web/helpers/icon_helper.ex
@@ -2,12 +2,11 @@ defmodule GoBarberWeb.Helpers.IconHelper do
   use Phoenix.HTML
 
   def fi_icon(icon, opts \\ []) do
-    classes = Keyword.get(opts, :class, "") <> " w-6 h-6 fi-icon"
+    classes = opts[:class] || ""
+    updated_opts = Keyword.put(opts, :class, classes <> " fi-icon")
 
-    content_tag :svg, class: classes do
-      tag(:use,
-        "xlink:href": "/images/feather-sprite.svg\##{icon}"
-      )
+    content_tag :svg, updated_opts do
+      tag(:use, "xlink:href": "/images/feather-sprite.svg\##{icon}")
     end
   end
 end
