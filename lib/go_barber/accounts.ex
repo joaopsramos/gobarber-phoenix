af58e24bc1df827b39a7f58e5e53b027c963be8f
João <joaopsramos231@gmail.com>
2021-05-25 16:43:00 -0300

    feat: authenticate user function created

--- lib/go_barber/accounts.ex
+++ lib/go_barber/accounts.ex
@@ -2,6 +2,10 @@ defmodule GoBarber.Accounts do
   alias GoBarber.Repo
   alias GoBarber.Accounts.User
 
+  def get_user_by(params) do
+    Repo.get_by(User, params)
+  end
+
   def register_user(attrs) do
     %User{}
     |> User.registration_changeset(attrs)
@@ -10,5 +14,21 @@ defmodule GoBarber.Accounts do
 
   def change_registration(%User{} = user, params) do
     User.registration_changeset(user, params)
+  end
+
+  def authenticate_user_by_email_password(email, pass) do
+    user = get_user_by(email: email)
+
+    cond do
+      user && Argon2.verify_pass(pass, user.password_hash) ->
+        {:ok, user}
+
+      user ->
+        {:error, :unauthorized}
+
+      true ->
+        Argon2.no_user_verify()
+        {:error, :not_found}
+    end
   end
 end
