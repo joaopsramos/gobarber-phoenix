59e474b8efa05bd8f512a5b800fec88e0151ed0b
João <joaopsramos231@gmail.com>
2021-05-25 17:08:27 -0300

    feat: session controller created

---
+++ lib/go_barber_web/controllers/session_controller.ex
@@ -0,0 +1,25 @@
+defmodule GoBarberWeb.SessionController do
+  use GoBarberWeb, :controller
+
+  alias GoBarber.Accounts
+  alias GoBarberWeb.Auth
+
+  def create(conn, %{"session" => %{"email" => email, "password" => pass}}) do
+    case Accounts.authenticate_user_by_email_password(email, pass) do
+      {:ok, user} ->
+        conn
+        |> Auth.login(user)
+        |> redirect(to: "/mydashboard")
+
+      {:error, _} ->
+        conn
+        |> redirect(to: "/signin")
+    end
+  end
+
+  def delete(conn, _params) do
+    conn
+    |> Auth.logout()
+    |> redirect(to: "/signin")
+  end
+end
