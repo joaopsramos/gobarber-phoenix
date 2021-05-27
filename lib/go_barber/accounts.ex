e33b97da7efeeedf0d84952dc5b7a424fae165ef
João <joaopsramos231@gmail.com>
2021-05-27 13:27:16 -0300

    style: accounts functions ordered alphabetically

--- lib/go_barber/accounts.ex
+++ lib/go_barber/accounts.ex
@@ -2,24 +2,6 @@ defmodule GoBarber.Accounts do
   alias GoBarber.Repo
   alias GoBarber.Accounts.User
 
-  def get_user(id) do
-    Repo.get(User, id)
-  end
-
-  def get_user_by(params) do
-    Repo.get_by(User, params)
-  end
-
-  def register_user(attrs) do
-    %User{}
-    |> User.registration_changeset(attrs)
-    |> Repo.insert()
-  end
-
-  def change_registration(%User{} = user, params) do
-    User.registration_changeset(user, params)
-  end
-
   def authenticate_user_by_email_password(email, pass) do
     user = get_user_by(email: email)
 
@@ -34,5 +16,23 @@ defmodule GoBarber.Accounts do
         Argon2.no_user_verify()
         {:error, :not_found}
     end
+  end
+
+  def change_registration(%User{} = user, params) do
+    User.registration_changeset(user, params)
+  end
+
+  def get_user(id) do
+    Repo.get(User, id)
+  end
+
+  def get_user_by(params) do
+    Repo.get_by(User, params)
+  end
+
+  def register_user(attrs) do
+    %User{}
+    |> User.registration_changeset(attrs)
+    |> Repo.insert()
   end
 end
