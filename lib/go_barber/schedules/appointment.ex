4e975bf129cb44b73e4d16290ca427b672c8dc1c
João <joaopsramos231@gmail.com>
2021-05-28 16:30:15 -0300

    feat: appointments schema updated
    customer_id and provider_id moved to optional attrs
    validate_hour and validate_past_date added

--- lib/go_barber/schedules/appointment.ex
+++ lib/go_barber/schedules/appointment.ex
@@ -3,11 +3,13 @@ defmodule GoBarber.Schedules.Appointment do
   import Ecto.Changeset
   alias GoBarber.Accounts.User
 
-  @required_attrs ~w(date provider_id customer_id)a
+  @required_attrs ~w(date)a
+  @optional_attrs ~w(customer_id provider_id)a
 
   @foreign_key_type :binary_id
   schema "appointments" do
     field :date, :utc_datetime
+
     belongs_to :customer, User
     belongs_to :provider, User
 
@@ -16,8 +18,42 @@ defmodule GoBarber.Schedules.Appointment do
 
   def changeset(appointment, attrs) do
     appointment
-    |> cast(attrs, @required_attrs)
+    |> cast(attrs, @required_attrs ++ @optional_attrs)
     |> validate_required(@required_attrs)
+    |> validate_hour()
+    |> validate_past_date()
     |> foreign_key_constraint(:provider_id)
   end
+
+  defp validate_hour(
+         %Ecto.Changeset{
+           valid?: true,
+           changes: %{date: %{hour: hour}}
+         } = changeset
+       ) do
+    if hour < 8 || hour > 17 do
+      add_error(changeset, :date, "hour must be between 8 and 17")
+    else
+      changeset
+    end
+  end
+
+  defp validate_hour(invalid_changeset), do: invalid_changeset
+
+  defp validate_past_date(
+         %Ecto.Changeset{
+           valid?: true,
+           changes: %{date: appointment_date}
+         } = changeset
+       ) do
+    current_date = DateTime.utc_now()
+
+    if DateTime.compare(appointment_date, current_date) == :lt do
+      raise "date can't be a past date"
+    else
+      changeset
+    end
+  end
+
+  defp validate_past_date(invalid_changeset), do: invalid_changeset
 end
