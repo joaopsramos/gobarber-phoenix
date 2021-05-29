8ea3d5d25e339af258443cdb739c75e488b46128
João <joaopsramos231@gmail.com>
2021-05-29 01:56:30 -0300

    test: appointments helpers functions added to test_helpers.ex

--- test/support/test_helpers.ex
+++ test/support/test_helpers.ex
@@ -1,5 +1,7 @@
 defmodule GoBarber.TestHelpers do
-  alias GoBarber.Accounts
+  alias Ecto.Multi
+  alias GoBarber.{Accounts, Schedules, Repo}
+  alias GoBarber.Schedules.Appointment
 
   def user_fixture(attrs \\ %{}) do
     {:ok, user} =
@@ -13,5 +15,48 @@ defmodule GoBarber.TestHelpers do
       |> Accounts.register_user()
 
     user
+  end
+
+  def appointment_fixture(provider_id, attrs \\ %{}) do
+    customer = attrs[:customer] || user_fixture()
+
+    attrs =
+      Enum.into(attrs, %{
+        date: attrs[:date] || ~U[2021-01-01 08:00:00.000000Z],
+        provider_id: provider_id
+      })
+
+    {:ok, appointment} = Schedules.create_appointment(customer, attrs)
+
+    appointment
+  end
+
+  def create_appointments_for_a_day(provider_id, year, month, day) do
+    %{id: customer_id} = user_fixture()
+
+    %{
+      scheduling_time: scheduling_time,
+      start_hour: start_hour,
+      end_hour: end_hour
+    } = Schedules.get_schedules_constants()
+
+    appointments_interval = start_hour..(end_hour - scheduling_time)
+
+    base_date = Date.new!(year, month, day)
+
+    appointments_interval
+    |> Enum.reduce(Multi.new(), fn hour, multi ->
+      date = DateTime.new!(base_date, Time.new!(hour, 0, 0))
+
+      changeset =
+        Schedules.change_appointment(%Appointment{}, %{
+          date: date,
+          provider_id: provider_id,
+          customer_id: customer_id
+        })
+
+      Multi.insert(multi, hour, changeset)
+    end)
+    |> Repo.transaction()
   end
 end
