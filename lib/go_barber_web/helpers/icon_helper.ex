293d47545e1e9390352b05f710bea768c99fa44c
João <joaopsramos231@gmail.com>
2021-06-01 00:15:26 -0300

    feat: box icons added

--- lib/go_barber_web/helpers/icon_helper.ex
+++ lib/go_barber_web/helpers/icon_helper.ex
@@ -1,6 +1,8 @@
 defmodule GoBarberWeb.Helpers.IconHelper do
   use Phoenix.HTML
 
+  @bi_types regular: "bx", solid: "bxs", logo: "bxl"
+
   def fi_icon(icon, opts \\ []) do
     classes = opts[:class] || ""
     updated_opts = Keyword.put(opts, :class, classes <> " fi-icon")
@@ -8,5 +10,15 @@ defmodule GoBarberWeb.Helpers.IconHelper do
     content_tag :svg, updated_opts do
       tag(:use, "xlink:href": "/images/feather-sprite.svg\##{icon}")
     end
+
+  def bi_icon(icon, opts \\ []) do
+    classes = opts[:class] || ""
+
+    icon_type = opts[:type] || :regular
+    prefix = @bi_types[icon_type]
+
+    attrs = Keyword.put(opts, :class, classes <> " bx #{prefix}-#{icon}")
+
+    content_tag(:i, "", attrs)
   end
 end
