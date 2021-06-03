11f979ba47c08e50f8c8bbbe42dd7cb9ed2fc2d9
João <joaopsramos231@gmail.com>
2021-06-03 00:38:58 -0300

    feat: alpinejs added

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,11 +14,21 @@ import "../css/app.scss"
 //
 import "phoenix_html"
 import {Socket} from "phoenix"
-import topbar from "topbar"
 import {LiveSocket} from "phoenix_live_view"
 
+import topbar from "topbar"
+import 'alpinejs'
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: {_csrf_token: csrfToken},
+  dom: {
+    onBeforeElUpdated(from, to) {
+      if(from.__x)
+        window.Alpine.clone(from.__x, to)
+    }
+  }
+})
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
@@ -33,4 +43,3 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
-
